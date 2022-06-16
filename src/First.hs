4724e54e764829d20f31300727ba72b701b791a1
yuuna seto <ys227513@univ.seitoku.ac.jp>
2022-06-16 16:23:24 +0900

    add myinit

---
+++ src/First.hs
@@ -0,0 +1,20 @@
+module First where
+
+double :: Int -> Int
+double x = x + x
+
+quadruple :: Int -> Int
+quadruple x = (double x)
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+average :: [Int] -> Int
+average xs = sum xs `div` length xs
+
+ave :: [Int] -> Int
+ave xs = div (sum xs) (length xs)
+
+myinit :: [a] -> [a]
+myinit (x:[]) = []
+myinit (x:xs) = x : (init xs)
